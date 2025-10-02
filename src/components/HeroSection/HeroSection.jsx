9cab2d47bd368acb497e3c4bd961d6bfd498118b
Athharv009 <162335912+Athharv009@users.noreply.github.com>
2025-10-02 10:23:12 +0530

    handle medical center

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,37 +1,35 @@
+import { Link } from "react-router";
 import NavBar from "../Navbar/NavBar";
 import styles from "./HeroSection.module.css";
 
 export default function HeroSection() {
   return (
-
-      <div className={styles.HeroSection}>
-        <div>
-          <NavBar />
+    <div className={styles.HeroSection}>
+      <div>
+        <NavBar />
+      </div>
+      <div className={styles.hero}>
+        <div className={styles.tagHero}>
+          <h2>
+            Skip the travel! Find Online <br />
+            <Link to='/find-doctors' style={{textDecoration: "none"}}>
+              <span className={styles.h1}>
+                Medical <span style={{ color: "#2AA7FF" }}>Centers</span>
+              </span>
+            </Link>
+          </h2>
+          <p>
+            Connect instantly with a 24x7 specialist or choose to video visit a
+            particular doctor.
+          </p>
+          <button>Find Centers</button>
         </div>
-        <div className={styles.hero}>
-            <div className={styles.tagHero}>
-              <h2>
-                Skip the travel! Find Online <br />
-                <span className={styles.h1}>
-                  Medical <span style={{ color: "#2AA7FF" }}>Centers</span>
-                </span>
-              </h2>
-              <p>
-                Connect instantly with a 24x7 specialist or choose to video
-                visit a particular doctor.
-              </p>
-              <button>Find Centers</button>
-            </div>
-            <div>
-              <div className={styles.heroImage}>
-                <img
-                  src={require("../../assets/hero-img.png")}
-                  alt="heroImage"
-                />
-              </div>
-            </div>
+        <div>
+          <div className={styles.heroImage}>
+            <img src={require("../../assets/hero-img.png")} alt="heroImage" />
+          </div>
         </div>
       </div>
-
+    </div>
   );
 }
